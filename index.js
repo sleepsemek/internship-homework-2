6f442dbce417e5999c332db948a7d6370acb6792
sleepsemek <bayukovvdaniil@gmail.com>
2025-11-01 18:59:32 +0300

    Добавлено визуальное разделение на задачи

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+/* Задание 1 - каррирование функции с любым числом аргументов */
+
+const curry = (fn) => {
+    const curried = (...args) => {
+        if (fn.length <= args.length) return fn.apply(this, args)
+
+        return (...rest) => curried(...args, ...rest)
+    }
+
+    return curried
+}
+
+const logFourArgs = (a, b, c, d) => console.log(a, b, c, d)
+const curriedLogFourArgs = curry(logFourArgs)
+
+curriedLogFourArgs("test")
+curriedLogFourArgs("a", "b", "c", "d")
+curriedLogFourArgs("a")("b")("c")("d")
+curriedLogFourArgs("a", "b")("c")("d")
+curriedLogFourArgs("a")("b", "c", "d")
+
+const sum = (a, b, c) => a + b + c
+const curriedSum = curry(sum)
+
+console.log(curriedSum(2)(3)(5))
+
+/* Задание 2 - преобразование строки в объект, разделяя свойства по точке */
+
 const nestedObjectFromDottedString = (dottedString) => {
     const keys = dottedString.split(".")
     const result = {}
@@ -13,22 +41,3 @@ const nestedObjectFromDottedString = (dottedString) => {
 }
 
 console.dir(nestedObjectFromDottedString('one.two.three.four.five'), {depth: null})
-
-const curry = (fn) => {
-    const curried = (...args) => {
-        if (fn.length <= args.length) return fn.apply(this, args)
-
-        return (...rest) => curried(...args, ...rest)
-    }
-
-    return curried
-}
-
-const logFourArgs = (a, b, c, d) => { console.log(a, b, c, d) }
-const curriedLogFourArgs = curry(logFourArgs)
-
-curriedLogFourArgs("test")
-curriedLogFourArgs("a", "b", "c", "d")
-curriedLogFourArgs("a")("b")("c")("d")
-curriedLogFourArgs("a", "b")("c")("d")
-curriedLogFourArgs("a")("b", "c", "d")
