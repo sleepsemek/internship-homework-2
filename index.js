161d705ca0d8487fe84c1ad0a0f949264b95f733
sleepsemek <bayukovvdaniil@gmail.com>
2025-11-01 18:39:25 +0300

    Initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Happy developing ✨')
+const nestedObjectFromDottedString = (dottedString) => {
+    const keys = dottedString.split(".")
+    const result = {}
+
+    let currentObject = result
+
+    for (const key of keys) {
+        currentObject[key] = {}
+        currentObject = currentObject[key]
+    }
+
+    return result
+}
+
+console.dir(nestedObjectFromDottedString('one.two.three.four.five'), {depth: null})
+
+const curry = (fn) => {
+    const curried = (...args) => {
+        if (fn.length <= args.length) return fn.apply(this, args)
+
+        return (...rest) => curried(...args, ...rest)
+    }
+
+    return curried
+}
+
+const logFourArgs = (a, b, c, d) => { console.log(a, b, c, d) }
+const curriedLogFourArgs = curry(logFourArgs)
+
+curriedLogFourArgs("test")
+curriedLogFourArgs("a", "b", "c", "d")
+curriedLogFourArgs("a")("b")("c")("d")
+curriedLogFourArgs("a", "b")("c")("d")
+curriedLogFourArgs("a")("b", "c", "d")
